2fbd3b99f6846f3f6b98281d3d58dd3b8cefb2fd
ChrisE217 <chris.ewart1999@gmail.com>
2024-05-27 11:33:04 +0100

    year tracker script

---
+++ scripts/yearTracker.js
@@ -0,0 +1,22 @@
+const currentYear = new Date().getFullYear();
+const startOfYear = new Date(`${currentYear}-01-01T00:00:00+00:00`).getTime();
+const endOfYear = new Date(`${currentYear}-12-31T23:59:59+00:00`).getTime();
+
+const progressOfThisYear = (Date.now() - startOfYear) / (endOfYear - startOfYear);
+
+const progressBarOfThisYear = () => {
+    const passedProgressBarIndex = parseInt(progressOfThisYear * 30);
+    return `{ ${'█'.repeat(passedProgressBarIndex)}${'▁'.repeat(30 - passedProgressBarIndex)} }`;
+};
+
+const readme = `\
+### Hi there 👋
+
+⏳ Year progress ${progressBarOfThisYear()} ${(progressOfThisYear * 100).toFixed(2)} %
+
+---
+
+⏰ Last updated on ${new Date().toUTCString()}\
+`
+
+console.log(readme)
